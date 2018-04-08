4b036cc4f2f3a480f9e8fd378a5d0eaecba0abdc
Rahul Janardhanan <rahul25jana@gmail.com>
2018-04-07 23:10:42 -0500

    updated controller of the ship
    speed is set --> restricting the player by not going outside the play space.

--- ShipController.cs
+++ ShipController.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class Control_Player : MonoBehaviour {
 
-	public float speed = 15.0f;
+	public float speed = 30.0f;
+	
+	float min_x = -5f;
+	float max_x = 5f;
+	public float pad = 1f;
 	
 	// Use this for initialization
 	void Start () 
 	{
 	
+	float Z_distance = transform.position.z - Camera.main.transform.position.z;
+	Vector3 left_most = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Z_distance )); // takes in world point and returns the world point coordinate // should find the distance
+	Vector3 right_most = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, Z_distance )); // takes in world point and returns the world point coordinate
+	//Vector3 upper_most = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Z_distance ));
+//	Vector3 down_most = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Z_distance ));
+		
+		min_x  = left_most.x +  pad;
+		max_x = right_most.x -  2 * pad;                                                  
 		
 	}
 	
@@ -44,12 +56,19 @@ public class Control_Player : MonoBehaviour {
 		
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
-			Vector3 ShipPos = this.transform.position;
+			Vector3 ShipPos = this.transform.position; 
 			ShipPos.y += -speed * Time.deltaTime;
 			this.transform.position = ShipPos;
 		}
 		
+		// will restrict the player in game space
+		float X_new = Mathf.Clamp(transform.position.x, min_x, max_x);
+		transform.position = new Vector3(X_new, transform.position.y, transform.position.z); // will reset the transform
+		//float X_news = Mathf.Clamp(transform.position.y, min_x, max_x);  // extra --> moving y axis
+		//transform.position = new Vector3(transform.position.x, X_news, transform.position.z); 
 		
 	}
+	
+	
 	
 }
