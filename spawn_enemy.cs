c3c0d51dd4902901b7fe5ee9bcdf0869c9ec4c36
Rahul Janardhanan <rahul25jana@gmail.com>
2018-04-09 19:56:36 -0500

    update v1
    restricts the enemies in particular radius and moves the enemy in left and right direction.

--- spawn_enemy.cs
+++ spawn_enemy.cs
@@ -5,12 +5,30 @@ public class Spawn_Enemy : MonoBehaviour {
 
 public GameObject enemy_prefab;
 
+public float W_width = 10f;
+public float H_height = 8f;
+public float Enemy_Speed = 8f;
+
+private bool Right_move = true;
+private float X_min;
+private float X_max;
+
+	
+
+ 
+	
 	// Use this for initialization
 	void Start () {
 	
-
-	
-	foreach(Transform child in transform )
+		float ZZ_distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 left_edge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0 , ZZ_distance));
+		Vector3 right_edge = Camera.main.ViewportToWorldPoint(new Vector3(1, 0 , ZZ_distance));
+		
+		X_min = left_edge.x;
+		X_max = right_edge.x;
+		                                                     
+		
+		foreach(Transform child in transform )
 	
 	{
 			GameObject enemy = Instantiate(enemy_prefab, child.transform.position, Quaternion.identity) as GameObject;
@@ -19,8 +37,44 @@ public GameObject enemy_prefab;
 	
 	}
 	
+	public void onDrawGizmos()
+	
+	{
+
+Gizmos.DrawWireCube(transform.position, new Vector3(W_width, H_height));
+	
+	}
+	
+	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	
+	{
+	
+	if (Right_move)
+	
+	{
+	transform.position +=  new Vector3(Enemy_Speed * Time.deltaTime, 0);
+	}
+	
+	else {
+			transform.position +=  new Vector3(-Enemy_Speed * Time.deltaTime, 0);
+	
+	}
+	
+	float right_edge_form = transform.position.x + (0.5f * W_width);
+	float left_edge_form = transform.position.x + (0.5f * W_width);
+	if ( left_edge_form < X_min )
+	
+	{
+	Right_move =true;
+	
+		} else if (right_edge_form > X_max)
+		
+		{
+			Right_move = false;
+		}
+	
 	
 	}
 }
