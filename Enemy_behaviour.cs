412cea905350fbfc3749c030ccbe321d7329d7dc
Rahul Janardhanan <rahul25jana@gmail.com>
2018-05-21 20:30:24 -0500

    Adding enemy shooting power
    In this version, enemy can shoot back the player with the random velocity.

--- Enemy_behaviour.cs
+++ Enemy_behaviour.cs
@@ -6,7 +6,25 @@ public class Enemy_Behaviour : MonoBehaviour {
 	// Use this for initialization
 	
 	public float health = 200;
-
+	public GameObject projectile;
+	public float projectileSpeed = 10f;
+	public float health = 150f;
+	public float shotsPerSecond = 0.5f;
+	
+	void Update(){
+ 		float prob = shotsPerSecond * Time.deltaTime;
+ 		if(Random.value < prob){
+ 			Fire ();
+ 		}
+ 	}
+ 	
+ 	void Fire(){
+ 		Vector3 offset = new Vector3(0, -1.0f, 0);
+ 		Vector3 firePos = transform.position + offset;
+ 		GameObject laser = Instantiate(projectile, firePos, Quaternion.identity) as GameObject;
+ 		laser.rigidbody2D.velocity = new Vector2(0,-projectileSpeed);
+ 	}
+ 	
 
 void OnTriggerEnter2D(Collider2D colld)
 
